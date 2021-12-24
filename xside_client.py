a8bc3c493c15a831f5f10bbe88706f152ae61fa9
Alexander Filimonov <me@igeophysix.com>
2021-12-24 09:09:04 +0300

    added initialization of the client added pagination for get requests

--- xside_client.py
+++ xside_client.py
@@ -5,15 +5,21 @@ import requests
 from PIL import Image
 
 import s3_client
-from settings import XSIDE_HOST, XSIDE_USER, XSIDE_PASSWORD
 
 logger = logging.getLogger(__name__)
 
 
 class XSide:
     """
-    2XSide client class
+    2XSide client class that gets clients, items and item files
     """
+
+    def __init__(self, host, email, password):
+        self._host = host
+        self._email = email
+        self._password = password
+        self.__tokens = {}
+        self._authenticate()
 
     class ClientError(Exception):
         pass
@@ -24,13 +30,9 @@ class XSide:
     class TokenExpiredException(Exception):
         pass
 
-    def __init__(self):
-        self.__tokens = {}
-        self._authenticate()
-
     def _authenticate(self):
-        body = {"email": XSIDE_USER, "password": XSIDE_PASSWORD}
-        resp = requests.post(f"{XSIDE_HOST}/api/token/", json=body)
+        body = {"email": self._email, "password": self._password}
+        resp = requests.post(f"{self._host}/api/token/", json=body)
         if resp.status_code != 200:
             logger.error(resp.json())
         else:
@@ -39,7 +41,7 @@ class XSide:
 
     def _refresh_tokens(self):
         body = {"refresh": self.__tokens['refresh']}
-        resp = requests.post(f"{XSIDE_HOST}/api/refresh/", json=body)
+        resp = requests.post(f"{self._host}/api/refresh/", json=body)
         if resp.status_code != 200:
             logger.error(f"Token refresh failed: {resp.json()}. Trying to reauthenticate")
             self._authenticate()
@@ -64,21 +66,27 @@ class XSide:
 
         raise XSide.ClientError(f"Unexpected response from the server. Response code: {resp.status_code}. Message: {resp.json()}")
 
-    def get_clients(self, client_id: int = None):
+    def get_clients(self, client_id: int = None, page_size=100, page=0):
         """
         Get clients
         :param client_id: (optional) client_id to get
+        :params page_size: (optional)
+        :params page: (optional) page number starting from 0
         :return:
         """
-        return self.__get(f"{XSIDE_HOST}/api/client/{client_id or ''}")
+        params = {"page_size": page_size, "page": page}
+        return self.__get(f"{self._host}/api/client/{client_id or ''}", params=params)
 
-    def get_items(self, item_id: int = None):
+    def get_items(self, item_id: int = None, page_size=100, page=0):
         """
         Get items
-        :param item_id: (optional) item_id to retrieve s
+        :param item_id: (optional) item_id to retrieve
+        :params page_size: (optional)
+        :params page: (optional) page number starting from 0
         :return:
         """
-        return self.__get(f"{XSIDE_HOST}/api/item/{item_id or ''}")
+        params = {"page_size": page_size, "page": page}
+        return self.__get(f"{self._host}/api/item/{item_id or ''}", params=params)
 
     def fetch_image(self, path):
         """
@@ -91,11 +99,17 @@ class XSide:
 
 
 if __name__ == "__main__":
-    xside = XSide()
+    from settings import XSIDE_HOST, XSIDE_USER, XSIDE_PASSWORD
+
+    xside = XSide(host=XSIDE_HOST, email=XSIDE_USER, password=XSIDE_PASSWORD)
     print(xside.get_clients())
+    print(xside.get_clients(page_size=2))
+    print(xside.get_clients(page_size=2, page=1))
     print(xside.get_clients(1))
     print(xside.get_items())
     print(xside.get_items(52))
+    print(xside.get_items(page_size=1))
+    print(xside.get_items(page_size=1, page=1))
     image_data = xside.fetch_image(path='images/Client1/Test 6/whatsapp.png')
     image = Image.open(image_data)
     image.show()
